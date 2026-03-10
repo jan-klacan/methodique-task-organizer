e5395a35d3a5d0e48de8bcda57343aa1644e2fd0
Jan Klacan <156678842+jan-klacan@users.noreply.github.com>
2026-03-10 04:24:02 +0100

    Create Task and TodoList structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+struct Task {
+    description: String,
+    completed: bool,
+}
+
+impl Task {
+    fn new(description: String) -> Task {
+        Task {
+            description,
+            completed: false,
+        }
+    }
+}
+
+struct TodoList {
+    tasks: Vec<Task>,
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        TodoList { tasks: Vec::new() }
+    }
 }
